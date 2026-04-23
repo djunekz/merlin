35e51d7726b0af055f8421ed3864f2051f113c3c
djunekz <gab288.gab288@passinbox.com>
2026-04-23 14:00:18 +0700

    Added file colored tools

---
+++ core/merlincolor.py
@@ -0,0 +1,45 @@
+#
+# Color Code : Tools Bashxt
+# Github     : https://github.com/djunekz/bashxt
+# Install    : with termux-app-store (github.com/djunekz/termux-app-store)
+#
+
+# TEXT COLOR
+N='\33[0m'
+BL='\33[30m'
+R='\33[31m'
+G='\33[32m'
+Y='\33[33m'
+B='\33[34m'
+M='\33[35m'
+C='\33[36m'
+LG='\33[1;37m'
+DG='\33[1;90m'
+LR='\33[1;91m'
+LG='\33[1;92m'
+LY='\33[1;93m'
+LB='\33[1;94m'
+LM='\33[1;95m'
+LC='\33[1;96m'
+W='\33[1;97m'
+
+# BACKGROUND TEXT COLOR
+BGR='\33[41m'
+BGG='\33[42m'
+BGY='\33[43m'
+BGB='\33[44m'
+BGM='\33[45m'
+BGC='\33[46m'
+BGLR='\33[101m'
+BGLG='\33[102m'
+BGLY='\33[103m'
+BGLB='\33[104m'
+BGLM='\33[105m'
+BGLC='\33[106m'
+BGW='\33[107m'
+
+# TEXT
+bold='\33[1m'
+D='\33[2m'
+U='\33[4m'
+H='\33[8m'
